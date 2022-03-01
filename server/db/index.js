a1439799d7b66a2a06e07a50b5cd18ec9bfd10ce
Guille Ávila <guille9797@hotmail.com>
2022-03-01 17:13:13 +0100

    tried updating daysleftobook

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/pooeblo";
+const MONGO_URI = process.env.MONGODB_URI
 
 mongoose
   .connect(MONGO_URI)
--- server/routes/bookings.routes.js
+++ server/routes/bookings.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const Booking = require('../models/Booking.model')
+const Subscription = require('../models/Subscription.model')
 
 
 // --- GET ALL BOOKINGS ROUTE
@@ -18,9 +19,22 @@ router.post("/create", (req, res) => {
 
     const { subscription, entryDate, exitDate } = req.body
 
-    Booking
-        .create({ subscription, entryDate, exitDate })
-        .then(response => res.json(response))
+    const entry = new Date(entryDate)
+    const exit = new Date(exitDate)
+
+    const totalDays = (exit - entry) / (1000 * 3600 * 24)
+
+    console.log(entry, exit, totalDays)
+
+    const promise1 = Booking.create({ subscription, entryDate, exitDate })
+    const promise2 = Subscription.findByIdAndUpdate(subscription, { daysLeftToBook: totalDays }, { new: true })
+
+    Promise
+        .all([promise1, promise2])
+        .then(response => {
+            console.log(response)
+            res.json(response)
+        })
         .catch(err => res.status(500).json(err))
 })
 
